dfc40149478fb8793f7303806d88a8e5dd333b6c
benjamin-abebe <benjamin.abebe@stu.ocsb.ca>
2025-05-27 18:18:43 +0000

    innit

--- js/script.js
+++ js/script.js
@@ -1,12 +1,23 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Benjamin Abebe
+// Created on: Feb 2025
 // This file contains the JS functions for index.html
 
-/**
- * Displays an alert with the message "Hello, World!"
- */
+
 function myButtonClicked() {
-  alert("Hello, World!")
+  // input
+  const inputHours = parseFloat(document.getElementById("hours-input").value)
+  const inputWage = parseFloat(document.getElementById("wage-input").value)
+  const TAX_INCOME = 0.18
+
+  // process
+  const homeSalary = (inputHours * inputWage) * (1.00 - TAX_INCOME)
+  const payedTax = (inputHours * inputWage) * TAX_INCOME
+
+
+  // output
+  document.getElementById('answer').innerHTML =
+    `Your take-home pay will be: $${homeSalary.toFixed(2)}<br>
+    Total tax paid: $${payedTax.toFixed(2)}`
 }
